98d7271bfbd95a2b9f5aabdb0ae9686bd47a47df
David Whipple <dwhipp88@gmail.com>
2024-01-22 17:44:22 -0700

    attempting to make diagonal difference dynamic

--- Algorithms/diagonal-difference.js
+++ Algorithms/diagonal-difference.js
@@ -6,9 +6,9 @@ let matrixTwo = [
 ];
 
 let matrixThree = [
-    [36, 42, 21],
-    [16, 65, 84],
-    [94, 87, 97],
+    [3, 3, 3],
+    [3, 3, 3],
+    [3, 3, 3],
 ];
 
 let matrixThreeReversed = [
@@ -18,24 +18,24 @@ let matrixThreeReversed = [
 ];
 
 let matrixFour = [
-    [1, 1, 1, 1],
-    [1, 1, 1, 1],
-    [1, 1, 1, 1],
-    [1, 1, 1, 1],
+    [4, 4, 4, 4],
+    [4, 4, 4, 4],
+    [4, 4, 4, 4],
+    [4, 4, 4, 4],
 ];
 
 let matrixFive = [
-    [23, 56, 23, 65, 43],
-    [23, 54, 76, 54, 43],
-    [23, 43, 54, 26, 48],
-    [82, 59, 78, 23, 31],
-    [54, 34, 64, 76, 41],
+    [5, 5, 5, 5, 5],
+    [5, 5, 5, 5, 5],
+    [5, 5, 5, 5, 5],
+    [5, 5, 5, 5, 5],
+    [5, 5, 5, 5, 5],
 ];
 
 let matrixBroken = [
-    [4, 2, 5],
-    [2, 6, 4, 3, 1],
-    [4, 7, 9],
+    [1, 2, 3],
+    [1, 1, 3, 4, 5],
+    [3, 2, 1],
 ];
 
 function diagonalDifference(arr) {
@@ -48,43 +48,20 @@ function diagonalDifference(arr) {
     console.log(Math.abs(diagResult));
 }
 
-// diagonalDifference(matrixFive);
-
 function matrixIteration(arr) {
-    // let arrSums = [0, 0];
-    // console.log('Reversed Array: ', reverseArr);
     let firstArrSum = 0;
     let secondArrSum = 0;
-    console.log(arr);
 
     for (let i = 0; i < arr.length; i++) {
-        console.log('Matrix 3: ', arr[i][i]);
         firstArrSum += arr[i][i];
-
-        // for (let j = 0; j < arr.length; j++) {
-        //     console.log('J: ', arr[j]);
-        // }
     }
-    console.log(firstArrSum);
 
     const reverseArr = arr.reverse();
 
     for (let j = 0; j < reverseArr.length; j++) {
-        console.log('3 Reversed: ', reverseArr[j][j]);
         secondArrSum += reverseArr[j][j];
     }
-    console.log(secondArrSum);
     console.log(firstArrSum + secondArrSum);
 }
 
-matrixIteration(matrixThree);
-
-// var printArray = function (arr) {
-//     if (typeof arr == 'object') {
-//         for (var i = 0; i < arr.length; i++) {
-//             printArray(arr[i]);
-//         }
-//     } else document.write(arr);
-// };
-
-// printArray(matrixFive);
+matrixIteration(matrixBroken);
